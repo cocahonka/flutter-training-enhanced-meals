7f00b3cb2d605656738b449b7e9d4187c92cc43a
Ivan <cocahonka@gmail.com>
2023-11-19 22:32:42 +0300

    added meal scope in tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_enhanced/scopes/categories_scope.dart';
+import 'package:meals_enhanced/scopes/meals_scope.dart';
 import 'package:meals_enhanced/screens/categories_screen.dart';
 
 void main() {
@@ -17,13 +18,15 @@ class MealApp extends StatelessWidget {
       seedColor: const Color.fromARGB(255, 131, 57, 0),
     );
 
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        colorScheme: colorScheme,
-        textTheme: GoogleFonts.latoTextTheme(),
-      ),
-      home: const CategoriesScope(
-        child: CategoriesScreen(),
+    return CategoriesScope(
+      child: MealsScope(
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            colorScheme: colorScheme,
+            textTheme: GoogleFonts.latoTextTheme(),
+          ),
+          home: const CategoriesScreen(),
+        ),
       ),
     );
   }
