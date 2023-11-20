2580d65c99d5bf5a3d6d9cb6e2b675bf06ef8c3b
Ivan <cocahonka@gmail.com>
2023-11-20 19:59:00 +0300

    made up filters swithers invidual updates

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_enhanced/extensions/change_notifier_selector.dart';
+import 'package:meals_enhanced/scopes/filters_scope.dart';
 import 'package:meals_enhanced/widgets/filter_switch.dart';
 
 class FiltersScreen extends StatelessWidget {
@@ -6,6 +8,8 @@ class FiltersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final filters = FiltersScope.read(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your filters'),
@@ -17,26 +21,26 @@ class FiltersScreen extends StatelessWidget {
             FilterSwitch(
               title: 'Gluten-free',
               description: 'Only included gluten-free meals',
-              value: false,
-              onChanged: (value) {},
+              valueListenable: filters.select((controller) => controller.isGlutenFree),
+              onChanged: (value) => filters.isGlutenFree = value,
             ),
             FilterSwitch(
               title: 'Lactose-free',
               description: 'Only included lactose-free meals',
-              value: false,
-              onChanged: (value) {},
+              valueListenable: filters.select((controller) => controller.isLactoseFree),
+              onChanged: (value) => filters.isLactoseFree = value,
             ),
             FilterSwitch(
               title: 'Vegeterian',
               description: 'Only included vegeterian meals',
-              value: false,
-              onChanged: (value) {},
+              valueListenable: filters.select((controller) => controller.isVegeterian),
+              onChanged: (value) => filters.isVegeterian = value,
             ),
             FilterSwitch(
               title: 'Vegan',
               description: 'Only included vegan meals',
-              value: false,
-              onChanged: (value) {},
+              valueListenable: filters.select((controller) => controller.isVegan),
+              onChanged: (value) => filters.isVegan = value,
             ),
           ],
         ),
--- lib/widgets/filter_switch.dart
+++ lib/widgets/filter_switch.dart
@@ -1,38 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class FilterSwitch extends StatelessWidget {
   const FilterSwitch({
     required this.title,
     required this.description,
-    required this.value,
+    required this.valueListenable,
     required this.onChanged,
     super.key,
   });
 
   final String title;
   final String description;
-  final bool value;
+  final ValueListenable<bool> valueListenable;
   final ValueSetter<bool> onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return SwitchListTile(
-      title: Text(
-        title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-      ),
-      subtitle: Text(
-        description,
-        style: Theme.of(context).textTheme.labelMedium!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-      ),
-      activeColor: Theme.of(context).colorScheme.tertiary,
-      contentPadding: const EdgeInsets.only(left: 34, right: 22),
-      value: value,
-      onChanged: onChanged,
+    return ValueListenableBuilder(
+      valueListenable: valueListenable,
+      builder: (context, value, child) {
+        return SwitchListTile(
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          subtitle: Text(
+            description,
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          value: value,
+          onChanged: onChanged,
+        );
+      },
     );
   }
 }
