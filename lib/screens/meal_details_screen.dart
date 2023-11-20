c30ccccf7192adb5dcb35328796a5341baa2881d
Ivan <cocahonka@gmail.com>
2023-11-20 15:50:15 +0300

    connect meal details screen with favorite meals scope and added animation to toggle favorite

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_enhanced/models/meal.dart';
+import 'package:meals_enhanced/scopes/favorites_meals_scope.dart';
 import 'package:meals_enhanced/widgets/meal_details_list_info.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -8,8 +9,22 @@ class MealDetailsScreen extends StatelessWidget {
 
   final Meal meal;
 
+  void _onFavoriteToggle(BuildContext context) {
+    final isAlreadyFavorite = FavoritesMealsScope.read(context).toggleFavorite(meal);
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 2),
+        content: Text(isAlreadyFavorite ? 'Meal no longer a favorite' : 'Meal marked as favorite!'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isFavorite = FavoritesMealsScope.watch(context).contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -18,8 +33,22 @@ class MealDetailsScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite_outline),
+            onPressed: () => _onFavoriteToggle(context),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (child, animation) {
+                return ScaleTransition(
+                  scale: Tween<double>(begin: 0, end: 1).animate(
+                    CurvedAnimation(
+                      parent: animation,
+                      curve: Curves.fastOutSlowIn,
+                    ),
+                  ),
+                  child: child,
+                );
+              },
+              child: Icon(isFavorite ? Icons.favorite : Icons.favorite_outline, key: ValueKey(isFavorite)),
+            ),
           ),
         ],
       ),
