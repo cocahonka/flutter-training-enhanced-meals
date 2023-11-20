afc3fc49409cec814df02af1b7cc52b950400b17
Ivan <cocahonka@gmail.com>
2023-11-20 14:37:18 +0300

    remove second scaffold from categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,25 +9,20 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final categories = CategoriesScope.watch(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Categories'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20,
-            childAspectRatio: 3 / 2,
-          ),
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            final category = categories[index];
-            return CategoryItem(category: category);
-          },
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
         ),
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          final category = categories[index];
+          return CategoryItem(category: category);
+        },
       ),
     );
   }
